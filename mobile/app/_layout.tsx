e6f850c8d9c461953455e4f9bb1979f038294fc0
Mwai Joseph <joesoftmwai@gmail.com>
2025-08-29 18:06:36 +0300

    config: setting up clerk auth manager, added expo-secure-store

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,10 @@
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
+import { ClerkProvider } from '@clerk/clerk-expo'
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+     <ClerkProvider>
+        <Slot/>
+     </ClerkProvider>
+    );
 }
