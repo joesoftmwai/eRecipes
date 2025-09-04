5e6142fa48cd0741b85f1288cf5832f4248c20f4
Mwai Joseph <joesoftmwai@gmail.com>
2025-09-04 11:50:07 +0300

    customize the safearea background color

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -2,12 +2,14 @@ import { Slot } from "expo-router";
 import { ClerkProvider } from '@clerk/clerk-expo'
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import { SafeAreaView } from "react-native-safe-area-context";
+import { COLORS } from '../constants/colors';
+
 // import { SafeAreaView } from "react-native";
 
 export default function RootLayout() {
   return (
      <ClerkProvider tokenCache={tokenCache}>
-        <SafeAreaView style={{ flex: 1 }}>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.background }}>
           <Slot/>
         </SafeAreaView>
      </ClerkProvider>
